f1308eaec2ce87a3f6ef67ff027121f90b7db304
Leon Shimizu <lmshimizu@gmail.com>
2024-12-10 01:41:28 +1000

    Add POST /companies/:company_id/employees/import route for bulk employee import from Excel

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   resources :companies do
     resources :departments, only: [ :index, :create, :update, :destroy ]
-    resources :employees, only: [ :index, :create, :show, :update, :destroy ]
+    resources :employees, only: [ :index, :create, :show, :update, :destroy ] do
+      post :import, on: :collection
+    end
     resources :custom_columns, only: [ :index, :create, :update, :destroy ]
     resources :payroll_records, only: [ :index, :create, :update, :destroy ]
   end
